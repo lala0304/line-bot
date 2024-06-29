310ec119fa523f9a673790427596e8d9bdd1688e
lala0304 <judy888149@gmail.com>
2024-06-29 18:21:29 +0800

    Add detailed logging and temporary minute-based testing

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
-import logging  # 提供日誌功能
-from flask import Flask, request, abort  # Flask 是一個輕量級的 Web 框架，用於建立 Web 伺服器
-from linebot import LineBotApi, WebhookHandler  # Line Bot SDK，用於與 Line Messaging API 進行互動
-from linebot.exceptions import InvalidSignatureError  # 當 Webhook 請求的簽名無效時拋出的異常
-from linebot.models import TextSendMessage  # 用於創建發送消息的模型
-import schedule  # 用於設置和管理定時任務
-import time  # 提供時間相關的函數
-import threading  # 提供線程支持
-import os  # 提供與操作系統交互的功能
-from dotenv import load_dotenv  # 用於加載 .env 文件中的環境變數
+import logging
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import TextSendMessage
+import schedule
+import time
+import threading
+import os
+from dotenv import load_dotenv
 
-# 加載 .env 文件
-load_dotenv()
+load_dotenv()  # 加載 .env 文件
 
-# 設置 Flask 應用
 app = Flask(__name__)
 
 # 配置日誌
@@ -37,22 +35,17 @@ def home():
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # 獲取請求標頭中的 X-Line-Signature
     signature = request.headers['X-Line-Signature']
-    # 獲取請求正文
     body = request.get_data(as_text=True)
     try:
-        # 處理 webhook 主體
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
     return 'OK'  # 確保返回 200 狀態碼
 
-# 設置不同時段的喝水提醒訊息
 def send_drink_water_reminder(message):
     logger.info(f"Attempting to send message: {message}")
     try:
-        # 推送消息給所有用戶
         line_bot_api.broadcast(TextSendMessage(text=message))
         logger.info("Sent drink water reminder to all users")
     except Exception as e:
@@ -67,11 +60,9 @@ def schedule_task():
     schedule.every().day.at("13:00").do(send_drink_water_reminder, message='☕ 午餐後的一杯水，現在是下午1點，有助於消化，保持健康！')
     schedule.every().day.at("15:30").do(send_drink_water_reminder, message='🌟 下午茶時間到了，現在是下午3點半，來杯水，保持頭腦清醒，繼續高效工作！')
     schedule.every().day.at("17:30").do(send_drink_water_reminder, message='🌅 工作接近尾聲，現在是下午5點半，來杯水，給今天畫個完美的句號！')
+    schedule.every().day.at("18:30").do(send_drink_water_reminder, message='🌙 晚飯時間到了，現在是晚上7點，先來一杯水，幫助消化更健康！')
     schedule.every().day.at("19:00").do(send_drink_water_reminder, message='🌙 晚飯時間到了，現在是晚上7點，先來一杯水，幫助消化更健康！')
     schedule.every().day.at("21:30").do(send_drink_water_reminder, message='🌜 現在是晚上9點半，睡前喝杯水，保持身體水分充足，迎接美好的夢境！')
-    
-    # 臨時測試：每分鐘發送一次消息
-    schedule.every().minute.at(":00").do(send_drink_water_reminder, message='這是一條測試消息。')
 
     while True:
         logger.info("Checking scheduled tasks")
